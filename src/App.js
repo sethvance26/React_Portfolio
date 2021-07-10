423bfcc6785fbb67cb1e34550c4633bb688dc3c8
Seth <sethvance26@gmail.com>
2021-07-10 02:46:54 -0400

    Creating a header component, & importing to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Header from './components/Header'
 
 function App() {
+
+  
   return (
-    <div className="App">
-     <h1>Hello From React</h1>
+    <div className="container">
+     <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>React Portfolio</h1>
+        </header>
+    )
+}
+
+export default Header
