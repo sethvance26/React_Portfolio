300b350376016cab8255c3266ff4dac3e8b6b4d4
Seth <sethvance26@gmail.com>
2021-07-16 01:50:17 -0400

    added About me component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Particles from 'react-particles-js';
 import Navbar from './components/Navbar.js';
 import Header from './components/Header';
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
     />
     <Navbar />
     <Header/>
+    <AboutMe />
     </>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import author from "../me.jpg"
+
+const AboutMe = () => {
+    return (
+        <div className="container py-5">
+            <div className="row g-0">
+                <div className="col-sm-6 col-md-8">
+                    <div className="photo-wrap mb-5">
+                    <img className="profile-img" src={author} alt="author.." />
+                </div>
+                </div>
+                <div className="col-lg-6 col-xm-12"></div>
+                <h1 className="about-heading">About Me</h1>
+
+                <p>A Tireless, results-driven Web Developer dedicated to building and optimizing user-focused websites for customers with various business and personal objectives. 
+                Judicious and creative when crafting effective websites, apps and platforms to propel growth and competitive advantage. 
+                Technically proficient and analytical problem solver with calm and focused demeanor. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
