b6595b2d33d6dc25153320b689d3b1b52a9d4513
Seth <sethvance26@gmail.com>
2021-07-18 13:27:04 -0400

    adding font awesome icons to services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDesktop } from '@fortawesome/free-solid-svg-icons'
+import { faFileCode, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Services = () => {
     return (
@@ -9,6 +12,7 @@ const Services = () => {
                    
                    <div className="col-md-3 col-sm-6">
                         <div className="box">
+                          <div className="circle"><FontAwesomeIcon icon={faDesktop} size="2x"/></div> 
                            <h3> Web Development </h3>
                            <p> Your application will be built with new proven technologies. </p>
                         </div>
@@ -16,6 +20,7 @@ const Services = () => {
                     
                     <div className="col-md-3 col-sm-6">
                         <div className="box">
+                        <div className="circle"><FontAwesomeIcon icon={faUser} size="2x"/></div>
                            <h3> Front-End </h3>
                            <p> Ensuring that user experience determines design choices. </p>
                         </div>
@@ -23,6 +28,7 @@ const Services = () => {
                     
                     <div className="col-md-3 col-sm-6">
                         <div className="box">
+                        <div className="circle"><FontAwesomeIcon icon={faFileCode} size="2x"/></div>
                            <h3> Back-End </h3>
                            <p> Building and maintaining servers, applications, and databases. </p>
                         </div>
