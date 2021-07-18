b3eaf7f8ba13c88f93aa5ac7680fcad49cb2f4d0
Seth <sethvance26@gmail.com>
2021-07-18 14:14:07 -0400

    adding portfolio component and images

---
+++ src/components/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+       <div className= "portfolio-wrapper">
+           <div className = "container">
+               <h1 className= "text-uppercase text-center py-5">Portfolio</h1>
+               <div className="image-box-wrapper row justify-content-center">
+                   <img className="portfolio-image" src="" alt=""></img>
+               </div>
+           </div>
+       </div>
+    )
+}
+
+export default Portfolio
