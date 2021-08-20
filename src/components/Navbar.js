456bdd785c808b736c776f789f24571d4271d8ae
Seth <sethvance26@gmail.com>
2021-08-20 19:59:49 -0400

    adding navbar scroll functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "../logo2.png"
+import {Link} from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
 <div className="container">
 
 
@@ -18,19 +19,19 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+              <Link smooth={true} to="home" offset={} className="nav-link" href="#">Home <span className="sr-only"></span></Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About Me</a>
+              <Link smooth={true} to="about" className="nav-link" href="#">About Me</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Portfolio</a>
+              <Link smooth={true} to="portfolio" className="nav-link" href="#">Portfolio</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Skills</a>
+              <Link smooth={true} to="services" className="nav-link" href="#">Services</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              <Link smooth={true} to="contact" className="nav-link" href="#">Contact</Link>
             </li>
             
           </ul>
